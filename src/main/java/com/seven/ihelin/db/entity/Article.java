f8a1534a170128ff853a567e24df27e5110d6959
iHelin <ihelin@outlook.com>
2016-12-26 22:39:49 +0800

    添加文章

---
+++ src/main/java/com/seven/ihelin/db/entity/Article.java
@@ -0,0 +1,85 @@
+package com.seven.ihelin.db.entity;
+
+import java.util.Date;
+
+public class Article {
+    private Integer id;
+
+    private String title;
+
+    private String author;
+
+    private String summary;
+
+    private Integer readNum;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private byte[] content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Integer getReadNum() {
+        return readNum;
+    }
+
+    public void setReadNum(Integer readNum) {
+        this.readNum = readNum;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+}
