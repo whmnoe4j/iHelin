8cfbfd7cd68ebf1a224fd9daa63049c223428475
iHelin <ihelin@outlook.com>
2017-02-12 23:12:28 +0800

    添加cookie测试

--- src/main/java/me/ianhe/controller/admin/AdminLoginController.java
+++ src/main/java/me/ianhe/controller/admin/AdminLoginController.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.UUID;
 
 @Controller
 public class AdminLoginController extends BaseAdminController {
@@ -22,21 +24,14 @@ public class AdminLoginController extends BaseAdminController {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "login", method = RequestMethod.GET)
-    public String loginPage(Model model, String from, HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if ("test".equals(cookie.getName())) {
-                // 如果user Cookie存在，进行处理
-                break;
-            }
-        }
+    public String loginPage(Model model, String from) {
         model.addAttribute("from", from);
         return "admin/login";
     }
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public String login(String username, String password, String from, HttpServletRequest request,
-                        HttpSession session, Model model) {
+                        HttpServletResponse response, HttpSession session, Model model) {
         if (username.equals(CommonConfig.getAdminUser()) && password.equals(CommonConfig.getAdminPassword())) {
             AdminUser adminUser = new AdminUser();
             adminUser.setAdminId(username);
@@ -45,15 +40,17 @@ public class AdminLoginController extends BaseAdminController {
             String rip = RequestUtil.getRealIp(request);
             adminUser.setLastLoginIp(rip);
             session.setAttribute(SESSION_KEY_ADMIN, adminUser);
+            Cookie cookie = new Cookie("ihelin", UUID.randomUUID().toString());
+            cookie.setMaxAge(60 * 60 * 24 * 7);
+            response.addCookie(cookie);
             LOGGER.info("Admin user: {} login success,ip : {}.", username, rip);
             if (StringUtils.isNotEmpty(from))
                 return "redirect:" + from;
             return "redirect:/admin/index";
-        } else {
-            model.addAttribute("error", "用户名密码不正确！");
-            model.addAttribute("from", from);
-            return ftl("login");
         }
+        model.addAttribute("error", "用户名密码不正确！");
+        model.addAttribute("from", from);
+        return ftl("login");
     }
 
     @RequestMapping(value = "logout", method = RequestMethod.GET)
