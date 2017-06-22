4287dec52e2101e99945542ab95db081d2e8557d
iHelin <ihelin@outlook.com>
2017-06-22 09:06:51 +0800

    bug修改

--- src/main/java/me/ianhe/utils/JSON.java
+++ src/main/java/me/ianhe/utils/JSON.java
@@ -35,7 +35,7 @@ public class JSON {
      */
     public static String toJson(Object object) {
         try {
-            OBJECT_MAPPER.writeValue(System.out, object);
+//            OBJECT_MAPPER.writeValue(System.out, object);
             return OBJECT_MAPPER.writeValueAsString(object);
         } catch (JsonGenerationException e) {
             LOGGER.error("encode(Object)", e);
