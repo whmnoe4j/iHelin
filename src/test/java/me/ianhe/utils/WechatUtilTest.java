bfbf3701f83feb4143b9474158d0df4d5cee6b1f
iHelin <ihelin@outlook.com>
2017-09-16 16:24:39 +0800

    test

--- src/test/java/me/ianhe/utils/WechatUtilTest.java
+++ src/test/java/me/ianhe/utils/WechatUtilTest.java
@@ -1,15 +1,18 @@
 package me.ianhe.utils;
 
+import org.jsoup.Jsoup;
 import org.junit.Test;
 
 /**
  * Created by iHelin on 17/4/23.
  */
 public class WechatUtilTest {
+
     @Test
     public void doGetStr() throws Exception {
-        String res = WechatUtil.doGetStr("https://www.ianhe.me/");
+        String res = WechatUtil.doGetStr("https://api.imjad.cn/hitokoto/");
         System.out.println(res);
+        System.out.println(Jsoup.connect("https://api.imjad.cn/hitokoto/").get().body().text());
     }
 
     @Test
