991cb85eef540b850c161afc84331f4fea516b38
iHelin <ihelin@outlook.com>
2017-09-21 22:00:42 +0800

    去除qq登录

--- src/main/java/me/ianhe/service/DailyService.java
+++ src/main/java/me/ianhe/service/DailyService.java
@@ -1,12 +1,6 @@
 package me.ianhe.service;
 
 import com.beust.jcommander.internal.Maps;
-import com.thankjava.wqq.SmartQQClient;
-import com.thankjava.wqq.consts.MsgType;
-import com.thankjava.wqq.entity.msg.SendMsg;
-import com.thankjava.wqq.entity.wqq.GroupInfo;
-import com.thankjava.wqq.entity.wqq.GroupsList;
-import me.ianhe.config.QQNotifyListener;
 import me.ianhe.dao.PoemMapper;
 import me.ianhe.db.entity.Poem;
 import me.ianhe.utils.DingUtil;
@@ -31,9 +25,6 @@ public class DailyService {
     @Autowired
     private PoemMapper poemMapper;
 
-    @Autowired
-    private QQNotifyListener listener;
-
     public void run() {
         dailyEnglish();
     }
@@ -42,7 +33,6 @@ public class DailyService {
         Poem poem = poemMapper.getByRandom();
         String msg = poem.getContent() + "      --" + poem.getTitle();
         DingUtil.say(msg);
-        sendqq(msg);
     }
 
     private void dailyEnglish() {
@@ -61,22 +51,6 @@ public class DailyService {
         String jsonData = JSON.toJson(data);
         logger.debug("每日一句：{}", jsonData);
         DingUtil.doSay(jsonData);
-        sendqq(resMap.get("content") + "    " + resMap.get("note"));
-    }
-
-    private void sendqq(String msg) {
-        SmartQQClient smartQQClient = listener.getSmartQQClient();
-        GroupsList groupsList = smartQQClient.getGroupsList(true);
-        logger.debug(JSON.toJson(groupsList.getGroups()));
-        Map<Long, GroupInfo> groupInfoMap = groupsList.getGroups();
-        for (Map.Entry<Long, GroupInfo> groupInfoEntry : groupInfoMap.entrySet()) {
-            if (groupInfoEntry.getValue().getName().equals("一点点学习小组")) {
-                long gid = groupInfoEntry.getKey();
-                SendMsg sendMsg = new SendMsg(gid, MsgType.group_message, msg);
-                smartQQClient.sendMsg(sendMsg);
-                logger.debug("定时发送");
-            }
-        }
     }
 
 }
