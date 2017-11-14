345c10988771f089a767db022098375ab4d9053f
Ian He <ihelin@outlook.com>
2017-11-14 23:08:45 +0800

    验证修改

--- src/test/java/me/ianhe/utils/CheckUtilTest.java
+++ src/test/java/me/ianhe/utils/CheckUtilTest.java
@@ -9,28 +9,28 @@ import java.io.File;
 /**
  * Created by iHelin on 17/4/13.
  */
-public class CryptUtilTest {
+public class CheckUtilTest {
 
     private static final String ORIGIN = "246260";
 
     @Test
     public void sha1() throws Exception {
-        System.out.println(CryptUtil.sha1(ORIGIN));
+        System.out.println(CheckUtil.sha1(ORIGIN));
     }
 
     @Test
     public void md5() throws Exception {
-        Assert.assertEquals(CryptUtil.md5(ORIGIN), CryptUtil.md5(ORIGIN.getBytes()));
+        Assert.assertEquals(CheckUtil.md5(ORIGIN), CheckUtil.md5(ORIGIN.getBytes()));
     }
 
     @Test
     public void md52() throws Exception {
-        System.out.println(CryptUtil.md5(new File(Global.getClassPath() + "/helloworld.xml")));
+        System.out.println(CheckUtil.md5(new File(Global.getClassPath() + "/helloworld.xml")));
     }
 
     @Test
     public void base64() throws Exception {
-        Assert.assertEquals(CryptUtil.decodeBase64(CryptUtil.encodeBase64(ORIGIN)), ORIGIN);
+        Assert.assertEquals(CheckUtil.decodeBase64(CheckUtil.encodeBase64(ORIGIN)), ORIGIN);
     }
 
 }
