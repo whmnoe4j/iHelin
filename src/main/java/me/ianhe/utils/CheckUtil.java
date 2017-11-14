94b309c65768420051aeb61e6a6ab63d70c29030
Ian He <ihelin@outlook.com>
2017-11-14 23:05:36 +0800

    验证修改

--- src/main/java/me/ianhe/utils/CheckUtil.java
+++ src/main/java/me/ianhe/utils/CheckUtil.java
@@ -1,16 +1,25 @@
 package me.ianhe.utils;
 
 import me.ianhe.exception.SystemException;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Base64;
 
 /**
- * 微信签名验证
+ * 验证
  *
  * @author iHelin
  * @since 2017/10/17 15:28
  */
 public class CheckUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CheckUtil.class);
 
     public static boolean checkSignature(String token,String signature, String timestamp, String nonce) {
         String[] arr = new String[]{token, timestamp, nonce};
@@ -21,8 +30,46 @@ public class CheckUtil {
             content.append(arr[i]);
         }
         // sha1加密
-        String temp = CryptUtil.sha1(content.toString());
+        String temp = sha1(content.toString());
         return temp.equals(signature);
+    }
+
+    public static String sha1(String s) {
+        return DigestUtils.sha1Hex(s);
+    }
+
+    public static String md5(byte[] content) {
+        return DigestUtils.md5Hex(content);
+    }
+
+    public static String md5(String s) {
+        return DigestUtils.md5Hex(s);
+    }
+
+    /**
+     * md5文件加密
+     *
+     * @param f
+     * @return
+     */
+    public static String md5(File f) {
+        try (FileInputStream fis = new FileInputStream(f)) {
+            byte[] bytes = DigestUtils.md5(fis);
+            return new String(bytes);
+        } catch (IOException e) {
+            LOGGER.error("md5 error!", e);
+            return null;
+        }
+    }
+
+    public static String encodeBase64(String s) {
+        byte[] bytes = Base64.getEncoder().encode(s.getBytes());
+        return new String(bytes);
+    }
+
+    public static String decodeBase64(String s) {
+        byte[] bytes = Base64.getDecoder().decode(s.getBytes());
+        return new String(bytes);
     }
 
     private CheckUtil() {
